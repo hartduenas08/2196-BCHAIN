1cf6e3b65e4e255189e4c376a810470f18d28414
hartduenas08 <103306862+hartduenas08@users.noreply.github.com>
2024-01-05 15:16:07 +0800

    Add files via upload

---
+++ Hello.jsx
@@ -0,0 +1,9 @@
+export default function Hello() {
+    let message = "I think I've done it";
+    return(
+        <div>
+            <p>Hello World!</p>
+            {/* <h1>{message}</h1> */}
+        </div>
+    );
+}
---
+++ Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Hello() {
+    let message = "I think I've done it";
+    return(
+        <div>
+            { <h1>{message}</h1> }
+            <p>Welcome, Sherwin Hart!</p>
+        </div>
+    );
+}
